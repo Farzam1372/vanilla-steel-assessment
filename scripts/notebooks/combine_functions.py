5591579e5b55f08621179848f9943585ea815b0f
Farzam Nazari <farzamnazari.mec@>
2025-09-07 21:43:26 +0200

    edite combine_functions.py to reudce and combine unnecessary lines

--- scripts/notebooks/combine_functions.py
+++ scripts/notebooks/combine_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+#clean supplier 1 dataset
 def clean_supplier1(df):
     df = df.copy()
     df.rename(columns={
@@ -14,16 +15,17 @@ def clean_supplier1(df):
         'Quantity': 'quantity'
     }, inplace=True)
 
-    df['weight_kg'] = df['weight_kg'].astype(str).str.replace(",", ".").astype(float)
-    df['thickness_mm'] = df['thickness_mm'].astype(str).str.replace(",", ".").astype(float)
-    df['width_mm'] = df['width_mm'].astype(str).str.replace(",", ".").astype(float)
+    df[["weight_kg", "thickness_mm", "width_mm"]] = df[["weight_kg", "thickness_mm", "width_mm"]].apply(
+    lambda col: col.astype(str).str.replace(",", ".").astype(float)
+    )
+
 
     df["material"] = None
     df["source"] = "supplier1"
 
     return df
 
-
+#clean supplier 2 dataset
 def clean_supplier2(df):
     df = df.copy()
     df.rename(columns={
@@ -35,10 +37,7 @@ def clean_supplier2(df):
 
     df['weight_kg'] = df['weight_kg'].astype(str).str.replace(",", ".").astype(float)
 
-    df["grade"] = None
-    df["finish"] = None
-    df["thickness_mm"] = None
-    df["width_mm"] = None
+    df[["grade", "finish", "thickness_mm", "width_mm"]] = None
     df["source"] = "supplier2"
 
     return df
